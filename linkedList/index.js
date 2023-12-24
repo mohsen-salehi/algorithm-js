6c7bbf5a2bd265a7b88554e9a71bef5826424199
mohsen <mohsensalehi2001@gmail.com>
2023-12-24 10:25:08 +0330

    add find insertAfter delete methud

--- linkedList/index.js
+++ linkedList/index.js
@@ -18,6 +18,68 @@ class LinkedList {
     }
   }
 
+  prepend(value) {
+    const newElement = { value: value, next: this.head };
+
+    this.head = newElement;
+
+    if (!this.tail) {
+      this.tail = newElement;
+    }
+  }
+
+  delete(value) {
+    if (!this.head) return;
+
+    while (this.head && this.head.value === value) {
+      this.head = this.head.next;
+    }
+
+    let currentElement = this.head;
+
+    while (currentElement.next) {
+      if (currentElement.next.value === value) {
+        currentElement.next = currentElement.next.next;
+      } else {
+        currentElement = currentElement.next;
+      }
+    }
+
+    if (this.tail.value === value) {
+      this.tail = currentElement;
+    }
+  }
+
+
+  insertAfter (value , afterValue) {
+    const existingElement = this.find(afterValue)
+    if(existingElement){
+      const newElement = {value : value , next : existingElement.value}
+
+      existingElement.next = newElement
+    }
+  }
+
+
+  find(value) {
+    if (!this.head) {
+      return;
+    }
+
+    let currentElement = this.head
+
+    while(currentElement){
+      if(currentElement.value === value){
+        return currentElement
+      }
+      currentElement = currentElement.next
+    }
+
+    return
+
+
+  }
+
   toArray() {
     const elements = [];
 
@@ -34,9 +96,21 @@ class LinkedList {
 
 const linkedList = new LinkedList();
 
-linkedList.append(2)
-linkedList.append("mohsen")
-linkedList.append(true)
+linkedList.append(2);
+linkedList.append("mohsen");
+linkedList.append("mohsen");
+linkedList.append(true);
+linkedList.prepend("FirstValue");
+linkedList.prepend("FirstValue");
+
+linkedList.delete("mohsen");
+linkedList.delete("FirstValue");
+linkedList.delete(true);
+console.log(linkedList.toArray());
+
+console.log(linkedList.find("mohsen"));
+console.log(linkedList.find(2 ));
 
 
+linkedList.insertAfter("new value " , 2)
 console.log(linkedList.toArray());
